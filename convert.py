e547b0329414c657e3204b9c529f97612397c392
EC2 Default User <ec2-user@ip-172-31-24-10.us-east-2.compute.internal>
2019-03-01 11:54:11 +0000

    Replaced xor with exponentiation

--- convert.py
+++ convert.py
@@ -2,18 +2,31 @@
 
 import sys
 
-print ("Number of arguments:", len(sys.argv), "arguments.")
-print ("Argument List:", str(sys.argv))
+#print ("Number of arguments:", len(sys.argv), "arguments.")
+#print ("Argument List:", str(sys.argv))
 
 if len(sys.argv) != 2:
     print("Incorrect Number of Arguments")
     sys.exit(1) 
 
 binary = sys.argv[1]
-print("Binary", list(binary))
+#print("Binary", list(binary))
 
 for digit in list(binary):
     if digit != "0" and digit != "1":
         print("Invalid Binary Number")
-        sys.exit(1) 
+        sys.exit(1)
+
+x=0
+result=0
+
+for digit in list(binary):
+    if digit == "1": 
+        result +=int(digit)*2**x
+        x+=1
+    
+print(result)
+
+
+
 
