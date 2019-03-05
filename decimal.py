2d4c14eb295f1324ebdd7040f1e001e680e6a996
EC2 Default User <ec2-user@ip-172-31-24-10.us-east-2.compute.internal>
2019-03-05 11:34:48 +0000

    Added file to system

---
+++ decimal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+#print ("Number of Arguments:", len(sys.agrv), "arguments.")
+#print ("Argument List:", str(sys.agrv))
+
+if len(sys.agrv) != 2:
+    print("Incorrect Number of Arguments")
+    sys.exit(1)
+
+
