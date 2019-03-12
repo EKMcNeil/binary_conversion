2ca3f6bdce4d4976a1d326e987a2518562d01574
EC2 Default User <ec2-user@ip-172-31-24-10.us-east-2.compute.internal>
2019-03-12 18:46:14 +0000

    Changed ways to iterate through range but needs more work

--- dec.py
+++ dec.py
@@ -11,13 +11,13 @@ if len(sys.argv) != 2:
 decimal = sys.argv[1]
 #print("Decimal", list(decimal))
 
-def test_range(decimal):
-    if decimal in range(0, 10,000):
-       print(" %s is a valid decimal")
-    else:
-       print("%s is not a valid decimal")
-    if not int(decimal):
-       print("%s is not a valid decimal")
+for digit in list(decimal):
+    print("Got digit:" + decimal)
+    if digit in range(0, 9):
+        print(" %s is a valid decimal")
+        sys.exit(1)
+
+sys.exit(0)
 
 max = 16
 
@@ -25,18 +25,25 @@ for n in range(0, max-1):
     if int(pow(2, n)) > int(decimal):
        print("1")
        m = n-1
-    if not int(pow(2, n)) < int(decimal):
+    elif int(pow(2, n)) < int(decimal):
        print("0") 
        break
+       exit
 
-second_digit = (int(decimal)%int(pow(2, n))
+second_digit = int(decimal)%int(pow(2, n))
 #second digit from the left
    
-if  int(pow(2, m)) > int(second_digit):
-    print("1")
-else:
-    print("0")
-    
+for h in range(0, max-1):
+    if int(pow(2, m)) > int(second_digit):
+       print("1")
+    elif int(pow(2, m)) < int(second_digit):
+       print("0")
+    break
+
+
+
+
+
 
 
 
