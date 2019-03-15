7a961f073094b742c094423f7efd5562507fece1
EC2 Default User <ec2-user@ip-172-31-24-10.us-east-2.compute.internal>
2019-03-15 12:42:51 +0000

    Fixed exceptions

--- dec.py
+++ dec.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 import sys
+
+#maximum number of bits converted 
+max = 16 
+
 #print ("Number of Arguments:", len(sys.argv), "arguments.")
 #print ("Argument List:", str(sys.argv))
 
@@ -12,12 +16,16 @@ decimal = sys.argv[1]
 #print("Decimal", list(decimal))
 
 for digit in list(decimal):
-    print("Got digit:" + decimal)
-    if digit in range(0, 9):
-        print(" %s is a valid decimal")
+    print("Got digit:" + digit)
+    if not digit in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+        print(decimal + " is not a valid decimal")
         sys.exit(1)
 
-sys.exit(0)
+#sys.exit(0)
+
+if int(decimal) > 2**max-1:
+    print(decimal + " is bigger than the maximum size " + str(2**max-1))
+    sys.exit(1)
 
 max = 16
 
@@ -32,6 +40,8 @@ for n in range(0, max-1):
 
 second_digit = int(decimal)%int(pow(2, n))
 #second digit from the left
+
+m = 0
    
 for h in range(0, max-1):
     if int(pow(2, m)) > int(second_digit):
