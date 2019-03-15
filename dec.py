35c5f1c64b9770e8c6bc28aaa24faa4f0739a19b
EC2 Default User <ec2-user@ip-172-31-24-10.us-east-2.compute.internal>
2019-03-15 20:21:23 +0000

    Updated the for loops with variable remainder

--- dec.py
+++ dec.py
@@ -25,30 +25,37 @@ for digit in list(decimal):
 
 if int(decimal) > 2**max-1:
     print(decimal + " is bigger than the maximum size " + str(2**max-1))
-    sys.exit(1)
+   #sys.exit(1)
 
 max = 16
+remainder = 0 
 
 for n in range(0, max-1):
     if int(pow(2, n)) > int(decimal):
        print("1")
        m = n-1
+       #what function does this m actually have???
     elif int(pow(2, n)) < int(decimal):
-       print("0") 
-       break
-       exit
-
-second_digit = int(decimal)%int(pow(2, n))
-#second digit from the left
-
-m = 0
-   
-for h in range(0, max-1):
-    if int(pow(2, m)) > int(second_digit):
-       print("1")
-    elif int(pow(2, m)) < int(second_digit):
        print("0")
+    remainder = (int(decimal)%int(pow(2 ,n)))
+    #Sets remainder variable for next loop 
     break
+    exit
+
+print(remainder)
+sys.exit(1)
+
+#hashed out until I remember why I set this variable! second_digit = int(decimal)%int(pow(2, n))
+#second digit from the left
+   
+for n in range(0, max-1):
+    if int(pow(2, n)) > int(remainder):
+       print("1")
+    elif int(pow(2, n)) < int(remainder):
+       print("0")
+    remainder = (int(remainder)%int(pow(2, n)))
+    break
+
 
 
 
