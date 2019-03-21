4a9d47aca53cd86c821f4b059fb5a2d7de6b73e5
EC2 Default User <ec2-user@ip-172-31-24-10.us-east-2.compute.internal>
2019-03-21 16:48:09 +0000

    Got decimal conversion working

--- dec.py
+++ dec.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 import sys
 
-#maximum number of bits converted 
-max = 16 
+#MAXimum number of bits converted 
+MAX = 16 
 
 #print ("Number of Arguments:", len(sys.argv), "arguments.")
 #print ("Argument List:", str(sys.argv))
@@ -16,51 +16,34 @@ decimal = sys.argv[1]
 #print("Decimal", list(decimal))
 
 for digit in list(decimal):
-    print("Got digit:" + digit)
+    #print("Got digit:" + digit)
     if not digit in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-        print(decimal + " is not a valid decimal")
+        #print(decimal + " is not a valid decimal")
         sys.exit(1)
 
-#sys.exit(0)
+if int(decimal) > 2**MAX-1:
+    print(decimal + " is bigger than the maximum size " + str(2**MAX-1))
+    sys.exit(1)
 
-if int(decimal) > 2**max-1:
-    print(decimal + " is bigger than the maximum size " + str(2**max-1))
-   #sys.exit(1)
+m = 0
+for n in range(0, MAX):
+    if 2**n > int(decimal):
+        m = n-1
+        break
+ 
+#print("m = " + str(m))
 
-max = 16
-remainder = 0 
+r = int(decimal)
+for i in reversed(range(0, m+1)):
+    #print("i = " + str(i))
+    t = r - 2**i
+    #print("t = " + str(t))
+#setting temporary variable   
+    if t >= 0:
+        r = t
+        print("1", end = "")
+    else:
+        print("0", end = "")
 
-for n in range(0, max-1):
-    if int(pow(2, n)) > int(decimal):
-       print("1")
-       m = n-1
-       #what function does this m actually have???
-    elif int(pow(2, n)) < int(decimal):
-       print("0")
-    remainder = (int(decimal)%int(pow(2 ,n)))
-    #Sets remainder variable for next loop 
-    break
-    exit
-
-print(remainder)
-sys.exit(1)
-
-#hashed out until I remember why I set this variable! second_digit = int(decimal)%int(pow(2, n))
-#second digit from the left
-   
-for n in range(0, max-1):
-    if int(pow(2, n)) > int(remainder):
-       print("1")
-    elif int(pow(2, n)) < int(remainder):
-       print("0")
-    remainder = (int(remainder)%int(pow(2, n)))
-    break
-
-
-
-
-
-
-
-
+print("")
 
