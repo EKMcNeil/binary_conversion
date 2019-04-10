875e5461ebeae88f7dbb7fc55c9e25e29ce7451a
EC2 Default User <ec2-user@ip-172-31-24-10.us-east-2.compute.internal>
2019-04-10 16:45:39 +0000

    Attempt to just get the program written but else: syntax is incorrect for unknown reason.

--- hex.py
+++ hex.py
@@ -12,6 +12,7 @@ if len(sys.argv) != 2:
 binary = sys.argv[1]
 #print("Binary", list(binary))
 
+#system check for binary digits only
 for digit in list(binary):
     if digit != "0" and digit != "1":
         print("Invalid Binary Number")
@@ -29,6 +30,7 @@ for digit in list(binary):
         first = x
     x += 1         
 
+#division into nybbles
 if x <= len(binary):
     nybbles.append(binary[first:])
 
@@ -37,6 +39,7 @@ print(nybbles)
 x=0
 result=0
 
+#binary to decimal conversion
 for digit in reversed(list(binary)):
     if digit == "1":
       result += int(digit)*2**x
@@ -47,15 +50,32 @@ print(result)
 a = 9
 x = 0
 m = 16
+list = ("a", "b", "c", "d", "e", "f", "g", "h")
+
+#simply getting the program written
+for x in list(result):
+    b = result % 16**x
+    if b == a:
+        print(next(list))
+    x += 1
+    a += 1
+    else:
+        print(b)
+    break
+
+sys.exit[1] 
+
+#tuple for values to use 'next' to iterate over. May not be correct
 hextuple = ("a", "b", "c", "d", "e", "f", "g", "h")
  
+#attempt to convert hex to decimal - syntax issues
 for x in list(result):
     b = result % 16**int(x)
     if b >= a:
         print(hextuple[1]) 
     a += 1
     x += 1
-    next(hextuple)
+    next(hextuple)#attempt to use the next letter in the tuple
     else:
         b < a:
         print(b)
