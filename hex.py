a61f9defb95552eef4e7d35294a380146bfe3dc7
EC2 Default User <ec2-user@ip-172-31-24-10.us-east-2.compute.internal>
2019-04-11 15:34:15 +0000

    Converted binary to hex with 0 padding

--- hex.py
+++ hex.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+#maybelittleendianstyle
+
 import sys
 
 #print ("Number of arguments:", len(sys.argv), "arguments.")
@@ -23,7 +25,8 @@ nybbles=[]
 first=0
 result = 0
 
-for digit in list(binary):
+for digit in (list(binary)):
+    print(digit)
     if x % 4 == 0:
         if x != 0:
             nybbles.append(binary[first:x])  
@@ -32,53 +35,48 @@ for digit in list(binary):
 
 #division into nybbles
 if x <= len(binary):
-    nybbles.append(binary[first:])
-
+    t = binary[first:]
+    r = len(binary) % 4
+    for i in range(4-r):
+        t += "0"        
+    nybbles.append(t)
+ 
 print(nybbles)
 
-x=0
-result=0
+hexnum = ""
 
-#binary to decimal conversion
-for digit in reversed(list(binary)):
-    if digit == "1":
-      result += int(digit)*2**x
-    x +=1
+for nybble in nybbles:
+    if nybble == "0000":
+        hexnum += "0"
+    elif nybble == "0001":
+        hexnum += "8"
+    elif nybble == "0010":
+        hexnum += "4"
+    elif nybble == "0011":
+        hexnum += "c"
+    elif nybble == "0100":
+        hexnum += "2"
+    elif nybble == "0101":
+        hexnum += "a"
+    elif nybble == "0110":
+        hexnum += "6"
+    elif nybble == "0111":
+        hexnum += "e"
+    elif nybble == "1000":
+        hexnum += "1"
+    elif nybble == "1001":
+        hexnum += "9"
+    elif nybble == "1010":
+        hexnum += "5"
+    elif nybble == "1011":
+        hexnum += "d"
+    elif nybble == "1100":
+        hexnum += "3"
+    elif nybble == "1101":
+        hexnum += "b"
+    elif nybble == "1110":
+        hexnum += "7"
+    elif nybble == "1111":
+        hexnum += "f"
 
-print(result)
-
-a = 9
-x = 0
-m = 16
-list = ("a", "b", "c", "d", "e", "f", "g", "h")
-
-#simply getting the program written
-for x in list(result):
-    b = result % 16**x
-    if b == a:
-        print(next(list))
-    x += 1
-    a += 1
-    else:
-        print(b)
-    break
-
-sys.exit[1] 
-
-#tuple for values to use 'next' to iterate over. May not be correct
-hextuple = ("a", "b", "c", "d", "e", "f", "g", "h")
- 
-#attempt to convert hex to decimal - syntax issues
-for x in list(result):
-    b = result % 16**int(x)
-    if b >= a:
-        print(hextuple[1]) 
-    a += 1
-    x += 1
-    next(hextuple)#attempt to use the next letter in the tuple
-    else:
-        b < a:
-        print(b)
-    break
- 
-print(b)
+print(hexnum)
