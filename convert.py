522ea82e879dc55efcf0d3a9d8fc2ca21d6bb2a9
EKMcNeil <47822656+EKMcNeil@users.noreply.github.com>
2019-06-17 20:45:50 +0100

    Adding function
    Compiling program parts into function to add to a library

--- convert.py
+++ convert.py
@@ -2,6 +2,20 @@
 
 import sys
 
+#converts binary to decimal
+def binary_to_decimal(binary): 
+    x = 0
+    result = 0
+    for digit in list(binary):
+        if digit == "1": 
+            result += int(digit) * 2**x 
+            #print(x, result)
+            x += 1
+        if digit == "0":
+            x +=1
+            #print(x, result)
+        return result
+
 #print ("Number of arguments:", len(sys.argv), "arguments.")
 #print ("Argument List:", str(sys.argv))
 
@@ -12,21 +26,12 @@ if len(sys.argv) != 2:
 binary = sys.argv[1]
 #print("Binary", list(binary))
 
+#system checks for correct values input
 for digit in list(binary):
     if digit != "0" and digit != "1":
         print("Invalid Binary Number")
         sys.exit(1)
 
-x = 0
-result = 0
-
-for digit in list(binary):
-    if digit == "1": 
-        result += int(digit) * 2**x 
-        #print(x, result)
-        x += 1
-    if digit == "0":
-        x +=1
-        #print(x, result)
+result = binary_to_decimal
          
 print(result)
