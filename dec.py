f49d622bef3ecb12568f0d1b11a36d4b01b583ba
Esther McNeil <esthermcneil@Esthers-Air.lan>
2019-08-07 20:23:38 +0100

    Adding classes to the dec.py file

--- dec.py
+++ dec.py
@@ -1,47 +1,51 @@
 #!/usr/bin/env python3
 
 import sys
+
+class InvalidError(Exception):
+    pass 
                                                
 #convert decimal to binary
 #max defines the maximum number of bits
-def decimal_to_binary(decimal, max = 16):
-    for digit in list(decimal):
-        #print("Got digit:" + digit)
-        if not digit in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-            raise ValueError(decimal + " is not a valid decimal")
+class DecimalToBinary:
+    def decimal_to_binary(self, decimal, max = 16):
+        for digit in list(decimal):
+            #print("Got digit:" + digit)
+            if not digit in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                raise ValueError(decimal + " is not a valid decimal")
 
-    if int(decimal) > (2**max) - 1:
-        print(decimal + " is bigger than the maximum size " + str(2**max-1))
-        sys.exit(1)
- 
-    r = int(decimal)
-    if r == 0:
-        return "0"
+        if int(decimal) > (2**max) - 1:
+            print(decimal + " is bigger than the maximum size " + str(2**max-1))
+            sys.exit(1)
+     
+        r = int(decimal)
+        if r == 0:
+            return "0"
 
-    m = 0
-    for n in range(0, max + 1):
-        if 2**n > int(decimal):
-            m = n
-            break
- 
-    #print("m = " + str(m))
+        m = 0
+        for n in range(0, max + 1):
+            if 2**n > int(decimal):
+                m = n
+                break
+     
+        #print("m = " + str(m))
 
-    binary = ""
+        binary = ""
 
-    for i in reversed(range(0, m)):
-        #print("i = " + str(i))
-        t = r - 2**i
-        #print("t = " + str(t))
-    #setting temporary variable   
-        if t >= 0:
-            r = t 
-            binary += "1"
-        else:
-            binary += "0"
-            
-    result = binary[::-1]      
-    
-    return result
+        for i in reversed(range(0, m)):
+            #print("i = " + str(i))
+            t = r - 2**i
+            #print("t = " + str(t))
+        #setting temporary variable   
+            if t >= 0:
+                r = t 
+                binary += "1"
+            else:
+                binary += "0"
+                
+        result = binary[::-1]      
+        
+        return result
 
 def main():
         #print ("Number of Arguments:", len(sys.argv), "arguments.")
